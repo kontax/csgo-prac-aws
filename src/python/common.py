78c33b2bd62f31baff9b5322c6cd59568a1b0ab4
James Coulson <github@coulson.ie>
2021-11-19 14:31:07 +0000

    Modify CORS settings for API

--- src/python/common.py
+++ src/python/common.py
@@ -14,5 +14,10 @@ def return_code(code, body):
 
     return {
         "statusCode": code,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         "body": json.dumps(body)
     }
